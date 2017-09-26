682833190b4dc1d85abc1919a55ade888fc2b0e7
deskach <dzianis.kachanau@gmail.com>
2017-09-27 04:28:53 +1000

    Assignement 1 done

---
+++ src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,17 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-warning-alert',
+  templateUrl: './warning-alert.component.html',
+  // styleUrls: ['./warning-alert.component.css']
+  styles: ['p {color: darkred;}']
+})
+export class WarningAlertComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
