a8045659018ee2f98f07b6a401ef54aa8873a5b4
deskach <dzianis.kachanau@gmail.com>
2017-09-29 02:04:42 +1000

    event binding example

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -13,8 +13,13 @@ import {Component} from '@angular/core';
 })
 export class ServersComponent {
   allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
 
   constructor() {
     setTimeout(_ => this.allowNewServer = true, 2000);
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created!';
   }
 }
