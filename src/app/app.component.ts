1a82004f272bab94881fe09c4ed8af4cd3cf7dd4
deskach <dzianis.kachanau@gmail.com>
2017-09-30 09:50:51 +1000

    61 - @Input() decorator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
+import {SECModel} from "./shared/models";
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,7 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  serverElements: SECModel[] = [
+    new SECModel('server', 'testServer', 'test'),
+  ];
 }
---
+++ src/app/shared/models.ts
@@ -0,0 +1,7 @@
+export class SECModel {
+  constructor(public type: string,
+              public name: string,
+              public content: string) {
+  }
+}
+
