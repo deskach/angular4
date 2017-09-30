8260d284a402e7b8ce8b13bb794aff0ad8911ce8
deskach <dzianis.kachanau@gmail.com>
2017-09-30 15:13:37 +1000

    74 - @ContentChild

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,15 +1,20 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {SECModel} from '../shared/server-element.model';
+import {AfterContentInit, Component, ContentChild, ElementRef, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css']
 })
-export class ServerElementComponent implements OnInit {
-  @Input() element: SECModel;
+export class ServerElementComponent implements OnInit, AfterContentInit {
+  @Input() name: string;
+  @ContentChild('contentParagraph') paragraph: ElementRef;
 
   ngOnInit() {
+    console.log('ngOnInit: ' + this.paragraph.nativeElement.textContent);
   }
 
+
+  ngAfterContentInit() {
+    console.log('ngAfterContentInit: ' + this.paragraph.nativeElement.textContent);
+  }
 }
