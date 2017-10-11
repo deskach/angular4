f852cbf40ceca8b0b6dda2b37581b8d6112fd226
deskach <dzianis.kachanau@gmail.com>
2017-10-12 09:00:37 +1100

    played with activated route

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-home',
@@ -7,10 +8,13 @@ import {Component, OnInit} from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() {
+  constructor(private _router: Router, private _activated: ActivatedRoute) {
   }
 
   ngOnInit() {
   }
 
+  onServers() {
+    this._router.navigate(['servers'], {relativeTo: this._activated});
+  }
 }
