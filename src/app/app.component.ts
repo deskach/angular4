91a5c100f0d636199bba0caae0d93da125206947
deskach <dzianis.kachanau@gmail.com>
2017-10-23 09:30:15 +1100

    added dynamic controls

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   genders = ['male', 'female'];
+  signupForm: FormGroup;
+
+  get hobbies(): FormArray {
+    return (<FormArray>this.signupForm.get('hobbies'));
+  }
+
+  ngOnInit() {
+    this.signupForm = new FormGroup({
+      'userData': new FormGroup({
+        'username': new FormControl(null, Validators.required),
+        'email': new FormControl(null, [Validators.required, Validators.email]),
+      }),
+      'gender': new FormControl('male'),
+      'hobbies': new FormArray([]),
+    });
+  }
+
+  handleAddHobby() {
+    this.hobbies.push(new FormControl(null, Validators.required));
+  }
+
+  onSubmit() {
+    console.log(this.signupForm.value);
+  }
 }
