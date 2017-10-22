b21f97e384c0577b22c632b6769a702128da10c2
deskach <dzianis.kachanau@gmail.com>
2017-10-23 10:55:37 +1100

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,21 +6,4 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  defaultSubscription = 'advanced';
-  @ViewChild('f') _form: NgForm;
-
-  _subscriptions = {
-    0: { value: 'basic', description: 'Basic', id: 0 },
-    1: { value: 'advanced', description: 'Advanced', id: 1 },
-    2: { value: 'pro', description: 'Pro', id: 2 },
-  };
-
-  get subscriptions() {
-    return Object.keys(this._subscriptions).map(k => this._subscriptions[k]);
-  }
-
-  onSubmit() {
-    console.log(this._form);
-  }
-
 }
