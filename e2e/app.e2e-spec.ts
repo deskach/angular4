70687f1fa214f2c59360919a03c6c32f18e2d41c
deskach <dzianis.kachanau@gmail.com>
2017-12-25 17:40:57 +1100

    Pipes - starting point

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
-import {AppPage} from './app.po';
+import {Ng4CompleteGuidePage} from './app.po';
 
-describe('angular4 App', () => {
-  let page: AppPage;
+describe('ng4-complete-guide App', function () {
+  let page: Ng4CompleteGuidePage;
 
   beforeEach(() => {
-    page = new AppPage();
+    page = new Ng4CompleteGuidePage();
   });
 
-  it('should display welcome message', () => {
+  it('should display message saying app works', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('app works!');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
 import {AppComponent} from './app.component';
-import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -11,6 +12,7 @@ import {FormsModule} from "@angular/forms";
   imports: [
     BrowserModule,
     FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
